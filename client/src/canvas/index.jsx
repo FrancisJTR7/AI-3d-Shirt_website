bd4a4f3f7cbee8f5244f5e635fe0f905a681e959
FrancisJTR7 <xgalasko@outlook.com>
2023-06-14 04:05:23 -0400

    optimized

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,50 @@
-import { Canvas } from '@react-three/fiber';
-import { Environment, Center } from '@react-three/drei';
+import React, { useEffect, useRef } from 'react';
+import { Canvas, useThree } from '@react-three/fiber';
+import { Center } from '@react-three/drei';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { EquirectangularReflectionMapping } from 'three';
 
 import Shirt from './Shirt';
 import Backdrop from './Backdrop';
 import CameraRig from './CameraRig';
+
+const loadEnvironmentMap = () => {
+  const loader = new RGBELoader();
+  return new Promise((resolve, reject) => {
+    loader.load(
+      '/sky.hdr',
+      (texture) => {
+        texture.mapping = EquirectangularReflectionMapping;
+        resolve(texture);
+      },
+      undefined,
+      (err) => {
+        reject(err);
+      }
+    );
+  });
+};
+
+const MyEnvironment = () => {
+  const { scene } = useThree();
+  const environmentMapRef = useRef();
+
+  useEffect(() => {
+    const loadMap = async () => {
+      try {
+        const environmentMap = await loadEnvironmentMap();
+        environmentMapRef.current = environmentMap;
+        scene.environment = environmentMap;
+      } catch (error) {
+        console.error('Error loading environment map:', error);
+      }
+    };
+
+    loadMap();
+  }, [scene]);
+
+  return null;
+};
 
 const CanvasModel = () => {
   return (
@@ -13,8 +54,8 @@ const CanvasModel = () => {
       gl={{ preserveDrawingBuffer: true }}
       className='w-full max-w-full h-full transition-all ease-in'
     >
-      <ambientLight intensity={0.5} />
-      <Environment preset='city' />
+      <ambientLight intensity={0.2} />
+      <MyEnvironment />
 
       <CameraRig>
         <Backdrop />
