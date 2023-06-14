1439c74205f56af669bba94796df28dc078c84e2
FrancisJTR7 <xgalasko@outlook.com>
2023-06-13 20:35:56 -0400

    all functional

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -56,15 +56,18 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://ai3js.onrender.com/api/v1/dalle', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        'https://threejsaishirtproject.onrender.com/api/v1/dalle',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       console.log('Response status:', response.status);
       console.log('Full response:', response);
