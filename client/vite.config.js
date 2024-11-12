39777c246e4bee40d887fcd872abeef9156d1765
FrancisJTR7 <xgalasko@outlook.com>
2024-11-12 16:39:24 -0500

    ok

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  alias: {
+    '@react-three/drei': '@react-three/drei/dist/index.cjs.js',
+  },
 });
