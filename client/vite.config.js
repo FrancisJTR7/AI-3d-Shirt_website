6858dc42b5ebf512c3c2f5a25b9faff682691fd4
FrancisJTR7 <xgalasko@outlook.com>
2024-11-12 16:55:32 -0500

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  resolve: {
-    alias: {
-      '@react-three/drei': require.resolve('@react-three/drei'),
-    },
-  },
-  optimizeDeps: {
-    include: ['@react-three/drei'],
-  },
-  ssr: {
-    noExternal: ['@react-three/drei'],
-  },
+  plugins: [react(), nodeResolve()],
 });
