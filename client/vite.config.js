b966100c5cc86849d315efca4a9650e9fdd988d1
FrancisJTR7 <xgalasko@outlook.com>
2024-11-12 16:15:57 -0500

    domain updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://3daishirt.com/',
+  base: 'https://ai-3d-shirt-website.vercel.app/',
 });
