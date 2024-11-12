c0e1fa5d5052ba84aa27035fb807a79c146a291c
FrancisJTR7 <xgalasko@outlook.com>
2024-11-12 16:21:57 -0500

    viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://ai-3d-shirt-website.vercel.app/',
+  base: '/',
 });
