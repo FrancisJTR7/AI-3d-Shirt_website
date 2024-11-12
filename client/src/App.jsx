768fe34ce1b0464d3b99778f6abbf2fe037604cb
FrancisJTR7 <xgalasko@outlook.com>
2024-11-12 16:27:52 -0500

    added router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Canvas from './canvas';
 import Customizer from './pages/Customizer';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <main className='app transition-all ease-in'>
-      <Home />
-      <Canvas />
-      <Customizer />
-    </main>
+    <BrowserRouter>
+      <main className='app transition-all ease-in'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/canvas' element={<Canvas />} />
+          <Route path='/customizer' element={<Customizer />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
