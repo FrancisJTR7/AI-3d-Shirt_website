bbb33667fbff3b9ba755283cfaf4559cc5e73c20
FrancisJTR7 <xgalasko@outlook.com>
2024-11-12 17:33:15 -0500

    fix app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Canvas from './canvas';
 import Customizer from './pages/Customizer';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <main className='app transition-all ease-in'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/canvas' element={<Canvas />} />
-          <Route path='/customizer' element={<Customizer />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <main className='app transition-all ease-in'>
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   );
 }
 
